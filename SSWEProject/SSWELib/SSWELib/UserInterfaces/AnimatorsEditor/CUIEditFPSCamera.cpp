79ac07c00de5ef2dc5bfc2be80a39e3831649995
julien-moreau <moreau-mathis@intechinfo.fr>
2013-09-17 00:10:49 +0200

    Added Rigged Camera
    Made:
    	- Added rigged camera, WIP
    	- Replacing FPS camera by Rigged Camera
    
    To Do:
    	- Work on rigged camera

--- SSWEProject/SSWELib/SSWELib/UserInterfaces/AnimatorsEditor/CUIEditFPSCamera.cpp
+++ SSWEProject/SSWELib/SSWELib/UserInterfaces/AnimatorsEditor/CUIEditFPSCamera.cpp
@@ -69,9 +69,12 @@ bool CUIEditFPSCamera::OnEvent(const SEvent &event) {
 				devices->getCollisionManager()->getFPSCameraSettings()->setSlidingValue(devices->getCore()->getF32(sv->getText()));
 				devices->getCollisionManager()->getFPSCameraSettings()->getCameraAnimator()->setMoveSpeed(devices->getCore()->getF32(ms->getText()));
 
-				devices->getCollisionManager()->createAnimatorCollisionCamera(devices->getFPSCamera());
-				devices->getSceneManager()->setActiveCamera(devices->getFPSCamera());
-				devices->getFPSCamera()->setPosition(devices->getMayaCamera()->getPosition());
+				//devices->getCollisionManager()->createAnimatorCollisionCamera(devices->getFPSCamera());
+				devices->getCollisionManager()->createAnimatorCollisionCamera(devices->getCameraRig()->getCameraSceneNode());
+				//devices->getSceneManager()->setActiveCamera(devices->getFPSCamera());
+				devices->getSceneManager()->setActiveCamera(devices->getCameraRig()->getCameraSceneNode());
+				//devices->getFPSCamera()->setPosition(devices->getMayaCamera()->getPosition());
+				devices->getCameraRig()->getCameraSceneNode()->setPosition(devices->getMayaCamera()->getPosition());
 				devices->getDevice()->getCursorControl()->setVisible(false);
 
 				devices->getEventReceiver()->RemoveEventReceiver(this);
